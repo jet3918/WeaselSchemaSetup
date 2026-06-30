44a1229d78b44790b9d97fba8c4a9295d619f851
jet3918 <101161807+jet3918@users.noreply.github.com>
2026-06-30 16:24:59 +0800

    Update PullSchemas.bat

--- Schemas/PullSchemas.bat
+++ Schemas/PullSchemas.bat
@@ -1,11 +1,11 @@
 @echo off
 echo 正在清理可能存在的旧版本...
-if exist rime-ice rmdir /s /q rime-ice
+if exist RimeIce rmdir /s /q RimeIce
 
 echo 正在克隆最新版雾凇拼音...
-git clone --depth=1 https://github.com/iDvel/rime-ice.git rime-ice
+git clone --depth=1 https://github.com/iDvel/rime-ice.git RimeIce
 
 echo 清理无用的 .git 隐藏文件夹（减小打包体积）...
-if exist rime-ice\.git rmdir /s /q rime-ice\.git
+if exist RimeIce\.git rmdir /s /q RimeIce\.git
 
 echo 拉取完成！
